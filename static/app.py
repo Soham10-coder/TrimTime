1aeb02ad49936654a3557b9d8b3dea85574950be
Soham10-coder <sohambagade1001@gmail.com>
2026-04-10 13:31:08 +0530

    fix render port

--- static/app.py
+++ static/app.py
@@ -456,4 +456,6 @@ def handle_message(data):
 
 # Run App
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
